41ad7585aa3c55e2d37079ddae6f731492b2466e
Francisco J. Casillas <chiccasillas@gmail.com>
2024-11-05 10:07:39 -0600

    Initial Commit

---
+++ Measure Converter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Measure Converter
+//
+//  Created by Francisco Jean on 05/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Measure Converter/Measure_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Measure_ConverterApp.swift
+//  Measure Converter
+//
+//  Created by Francisco Jean on 05/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Measure_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
